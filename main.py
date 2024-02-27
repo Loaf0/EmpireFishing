133a5d5a71b9a46da0b3c2e6489943bae79f7c19
Loaf0 <ninjakin102@outlook.com>
2024-02-27 15:16:54 -0500

    Register System 90% implemented

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from dateutil.utils import today
+from datetime import date
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
-import MySQLdb.cursors
 import pypyodbc as odbc  # pip install pypyodbc
 import re
 
@@ -13,10 +12,12 @@ server = 'empirefishing.database.windows.net'
 database = 'EmpireFishingCSCI-4485'
 dbusername = 'empirefishing'
 dbpassword = '@Stockton'
-connection_string = ('Driver={ODBC Driver 18 for SQL Server};Server=' + server + ',1433;Database=' + database + ';Uid=' + dbusername + ';Pwd=' + dbpassword + ';Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
+connection_string = (
+        'Driver={ODBC Driver 18 for SQL Server};Server=' + server + ',1433;Database=' + database + ';Uid=' + dbusername + ';Pwd=' + dbpassword + ';Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
 conn = odbc.connect(connection_string)
 
 mysql = MySQL(app)
+
 
 @app.route('/')
 def home():
@@ -37,13 +38,6 @@ def live_bait():
 def login():
     msg = ''
 
-    # debug
-    # cursor = conn.cursor()
-    # cursor.execute('SELECT * FROM userdata')
-    # Fetch one record and return result
-    # account = cursor.fetchone()
-    # print(account)
-
     # check if username and password were received
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
 
@@ -56,7 +50,7 @@ def login():
 
         # Fetch one record and return result
         account = cursor.fetchone()
-        print(account)
+
         if account:
             # Create session data, we can access this data in other routes
             session['loggedin'] = True
@@ -93,9 +87,55 @@ def profile():
     return render_template("profile.html", username=username, email=email, phone=phone)
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template("register.html")
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    if (request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in
+            request.form):
+
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        phone = request.form['phone']
+        if request.form['consent']:  # 1 yes 0 no
+            consent = 1
+        else:
+            consent = 0
+
+        # Check if account exists using MySQL
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM userdata WHERE username = ?', (username, ))
+        account = cursor.fetchone()
+
+        # If account exists show error and validation checks
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
+        elif len(password) < 3:
+            msg = 'Password too short!'
+        elif len(phone) < 12:
+            msg = 'Invalid Phone Number!'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form!'
+        else:
+            # Account doesn't exist and the form data is valid, now insert new account into accounts table
+            cursor.execute('INSERT INTO userdata VALUES ( ?, ?, ?, ?, ?, ?, ?, ?)',
+                           (username, password, email, consent, phone, 0, id, date.today()))
+            # today sets the account creation date, zero is for not admin
+            conn.commit()
+            msg = 'You have successfully registered!'
+
+    elif request.method == 'POST':
+        # Form is empty... (no POST data)
+        msg = 'Please fill out the form!'
+    # Show registration form with message (if any)
+    return render_template('register.html', msg=msg)
 
 
 if __name__ == '__main__':
